c24041f1f9028073be0a4d2e134653057cae9993
arkirollos <66964696+arkirollos@users.noreply.github.com>
2020-07-18 15:58:27 -0400

    new forecast in js,html, and css

--- src/index.js
+++ src/index.js
@@ -42,6 +42,19 @@ function currentDate(date) {
   return `${today} ${number} ${month}, ${hours}:${minutes}`;
 }
 
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${minutes}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
+
 let now = new Date();
 let date = document.querySelector("#full-date");
 date.innerHTML = currentDate(now);
@@ -78,12 +91,40 @@ function displaydata(response) {
     `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${response.data.weather[0].icon}.svg`
   );
 }
+function showForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  let forecast = null;
+  forecastElement.innerHTML = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+  <div class="col-2">
+    <h4>
+      ${formatHours(forecast.dt * 1000)}
+    </h4>
+    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+      forecast.weather[0].icon
+    }.svg"
+      />
+
+    <div class="forecast-description">${forecast.weather[0].description} </div>
+    <div class="forecast-temp"><strong>${Math.round(
+      forecast.main.temp_max
+    )}°</strong> ${Math.round(forecast.main.temp_min)}°</div>
+  </div>
+  `;
+  }
+}
+
 function toSearch(city) {
   let apiKey = "c3f2e22d068bc89a846c4b1c217f57c3";
   let units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-
   axios.get(apiUrl).then(displaydata);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
 }
 
 function handleSubmit(event) {
@@ -134,6 +175,7 @@ function showPosition(position) {
   console.log(apiUrl);
 
   axios.get(apiUrl).then(displaydata);
+  axios.get(apiUrl).then(showForecast);
 }
 
 function currentPosition(event) {
